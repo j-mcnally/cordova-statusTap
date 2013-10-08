e0563209df56ba6c96d08d4754f2d6bf596fb107
Andrew Trice <triceam@Andrew-Trices-MacBook-Pro.local>
2013-10-07 21:15:02 -0400

    updated JS interface/init

--- www/taptoscroll.js
+++ www/taptoscroll.js
@@ -6,20 +6,4 @@ var TapToScroll = function() {
 	exec(null, null, "TapToScroll", "initListener",[]);  
 }
 
-/*
-
-TapToScroll.prototype.initListener = function() {
-cordova.exec(null, null, "TapToScroll", "initListener",[]);
-};
-
-if(!window.plugins) {
-  window.plugins = {};
-}
-if(!window.plugins.tapToScroll) {
-  window.plugins.tapToScroll = new TapToScroll();
-}
-
-
-
 module.exports = new TapToScroll();
-*/
