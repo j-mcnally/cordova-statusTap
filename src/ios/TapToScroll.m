c3f5660c18d3608b2d1ea52e3bdbc7f28d3c9f44
Vitalik Zaidman <vzaidman@gmail.com>
2015-06-18 12:00:07 +0300

    using RotationLessViewController again
    bugs were found in case device rotation was prevented.

--- src/ios/TapToScroll.m
+++ src/ios/TapToScroll.m
@@ -8,6 +8,19 @@
 
 #import "TapToScroll.h"
 
+@interface RotationLessViewController : UIViewController
+@end
+
+@implementation RotationLessViewController
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation{
+  return NO;
+}
+-(BOOL)shouldAutorotate {
+  return  NO;
+}
+@end
+
+
 @implementation TapToScroll
 
 @synthesize recognizer;
@@ -31,8 +44,7 @@
             [tapRecognizer setNumberOfTouchesRequired:1];
             [self setRecognizer:tapRecognizer];
 
-            [overlay setRootViewController:[[UIViewController alloc] init]];
-            [overlay setBackgroundColor:[UIColor clearColor]];
+            [overlay setRootViewController:[[RotationLessViewController alloc] init]];
             [overlay setHidden:NO];
             
             [[[overlay rootViewController] view] setFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
@@ -48,28 +60,45 @@
     }];
 }
 
+-(void) tapped:(UITapGestureRecognizer *)sender {
+    NSLog(@"Status Bar Tapped Event");
+    [webView stringByEvaluatingJavaScriptFromString:@"var evt = document.createEvent(\"Event\"); evt.initEvent(\"statusTap\",true,true); window.dispatchEvent(evt);"];
+}
 
 -(void) setupRotationListener {
-  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationDidChange:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationDidChange:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
 }
 
 -(void) orientationDidChange: (NSNotification *)notification {
-  [self rotateToStatusBarFrame];
-}
-
--(void) tapped:(UITapGestureRecognizer *)sender {
-    NSLog(@"Status Bar Tapped Event");
-  [webView stringByEvaluatingJavaScriptFromString:@"var evt = document.createEvent(\"Event\"); evt.initEvent(\"statusTap\",true,true); window.dispatchEvent(evt);"];
-}
-
-
-- (void)rotateToStatusBarFrame {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [overlay setFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 20.0)];
-        [overlay setWindowLevel:(UIWindowLevelStatusBar+1.f)];
+        [self rotateToStatusBarFrame];
     });
 }
 
+- (void)rotateToStatusBarFrame {
+    float screenHeight = [UIScreen mainScreen].bounds.size.height;
+    float screenWidth = [UIScreen mainScreen].bounds.size.width;
+    float barHeight = 20; //barHeight is always 20 unless it is hidden or in a call: http://www.idev101.com/code/User_Interface/sizes.html
+    
+    CGRect frame;
+    
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (orientation == UIDeviceOrientationPortrait) {
+        frame = CGRectMake(0, 0, screenWidth, barHeight);
+    }
+    else if (orientation == UIDeviceOrientationLandscapeLeft) {
+        frame = CGRectMake(screenHeight - barHeight, 0, barHeight, screenWidth);
+    }
+    else if (orientation == UIDeviceOrientationLandscapeRight) {
+        frame = CGRectMake(0, 0, barHeight, screenWidth);
+    }
+    else if (orientation == UIDeviceOrientationPortraitUpsideDown) {
+        frame = CGRectMake(0, screenHeight - barHeight, screenWidth, barHeight);
+    }
+    
+    [overlay setFrame:frame];
+    [overlay setWindowLevel:(UIWindowLevelStatusBar+1.f)];
+}
 
 -(void) dealloc {
 #if ! __has_feature(objc_arc)
